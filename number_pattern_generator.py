5ff17554fbbd8d31389e31b38fe6c25d140b2264
Tran Quoc Hoang <tranquochoang.pv@protonmail.com>
2026-09-08 19:54:30 +0700

    feat: finish number pattern generator

---
+++ number_pattern_generator.py
@@ -0,0 +1,15 @@
+# A small app that creates a number pattern based on the input integer n, built in Python for freeCodeCamp's Number Pattern Generator lap.
+
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    if n < 1:
+        return "Argument must be an integer greater than 0."
+
+    result = ""
+    for i in range(1, n+1):
+        result += str(i)
+        if i < n:
+            result += " "
+    
+    return result
