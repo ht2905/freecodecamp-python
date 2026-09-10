08ab0005d4542f8147311899e2dede3733dc6e33
Tran Quoc Hoang <tranquochoang.pv@protonmail.com>
2026-09-11 02:17:01 +0700

    feat: add media catalogue workshop

---
+++ media_catalogue.py
@@ -0,0 +1,6 @@
+class Movie:
+    def __init__(self, title, year, director, duration):
+        self.title = title
+        self.year = year
+        self.director = director
+        self. duration = duration
